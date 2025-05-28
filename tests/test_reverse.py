6a7adc4b42dd34c904aaf6e6897ccb72344a4b43
Roman Pogoreltcev <romzes26@yandex.ru>
2025-05-28 16:50:03 +0700

    update test_reverse.py: add new tests

--- tests/test_reverse.py
+++ tests/test_reverse.py
@@ -4,3 +4,14 @@ def test_up_reverse():
     assert reverse_string('skypro') == 'orpyks'
     assert reverse_string('111') == '111'
     assert reverse_string('') == ''
+
+import pytest
+
+@pytest.mark.parametrize("string, expected_result", [
+    ("hello", "olleh"),
+    ("world", "dlrow"),
+    ("12345", "54321"),
+    ("", ""),
+])
+def test_reverse_string(string, expected_result):
+    assert reverse_string(string) == expected_result
